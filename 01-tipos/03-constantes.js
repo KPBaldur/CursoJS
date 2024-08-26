dda47eb6444b716726632ca97cb780470d184148
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-25 22:04:33 -0400

    Create 03-constantes.js

---
+++ 01-tipos/03-constantes.js
@@ -0,0 +1,9 @@
+/*let nombre = "Hola mundo";
+console.log(nombre)
+
+nombre = "Chango Banano"
+console.log(nombre)*/
+
+const nombreFijo = "Juancho";
+nombreFijo = "Pancracio";
+console.log(nombreFijo);
