7499ecb973f66ffcef59344010a390fc0a22b107
Kevin Pizarro <kevin.pizarrosanz@gmail.com>
2024-08-25 22:04:28 -0400

    Create 02-primitivos.js

---
+++ 01-tipos/02-primitivos.js
@@ -0,0 +1,15 @@
+let numero = 1;
+let texto = "Hola mundo";
+let verdadero = true;
+let falso = false;
+let noDefinido;
+let undef = undefined;
+let nulo = null;
+
+console.log(numero);
+console.log(texto);
+console.log(verdadero);
+console.log(falso);
+console.log(noDefinido);
+console.log(undef);
+console.log(nulo);
